0a51dbbd80c494a62cd333603db1b3931b58e974
sengayire <psengayire@gmail.com>
2023-06-19 13:52:18 +0100

    chore: add styles

---
+++ __tests__/app.tsx
@@ -0,0 +1,9 @@
+import { render } from '@testing-library/react-native'
+
+import App from '../App'
+
+describe('App component', () => {
+  it('render app', () => {
+    render(<App />)
+  })
+})
---
+++ __tests__/screens/index.tsx
@@ -0,0 +1,9 @@
+import { render } from '@testing-library/react-native'
+
+import Dashboard from 'src/screens/dashboard'
+
+describe('Dashboard component', () => {
+  it('render dashboard', () => {
+    render(<Dashboard />)
+  })
+})
