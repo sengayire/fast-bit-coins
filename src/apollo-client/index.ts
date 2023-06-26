f9c4a6f05881e5048a7e10d0f0075d09ac5274b8
sengayire <prince.s@cptim.com>
2023-06-26 23:28:46 +0100

    feat: add client

--- src/apollo-client/index.ts
+++ src/apollo-client/index.ts
@@ -2,10 +2,10 @@ import { ApolloClient, HttpLink, InMemoryCache } from '@apollo/client'
 import fetch from 'cross-fetch'
 
 const link = new HttpLink({
-  uri: 'https://resolved-hound-17.hasura.app/v1/graphql',
+  uri: 'https://bjsxxuwozdyhofdcguhi.hasura.eu-west-2.nhost.run/v1/graphql',
   headers: {
-    'Content-type': 'application/json',
-    'x-hasura-admin-secret': 'r1XeDufWr2bY8rW8WneXXVrZ559qDWqAHCoCrraxpUpM4K8QuE0YIbzMYlQxm3yC'
+    'Content-type': 'application/json'
+    // 'x-hasura-admin-secret': 'r1XeDufWr2bY8rW8WneXXVrZ559qDWqAHCoCrraxpUpM4K8QuE0YIbzMYlQxm3yC'
   },
   fetch
 })
--- src/queries/index.ts
+++ src/queries/index.ts
@@ -2,28 +2,22 @@ import { gql } from '@apollo/client'
 
 export const USER_LOGIN = gql`
   mutation LoginUser(
-    $country: String
-    $email: String
-    $password: String
-    $passwordConfirm: String
+    $country: String!
+    $email: String!
+    $password: String!
+    $passwordConfirm: String!
     $state: String
   ) {
-    insert_login(
-      objects: {
-        country: $country
-        email: $email
-        password: $password
-        passwordConfirm: $passwordConfirm
-        state: $state
-      }
+    login(
+      country: $country
+      email: $email
+      password: $password
+      passwordConfirm: $passwordConfirm
+      state: $state
     ) {
-      returning {
-        email
-        password
-        passwordConfirm
-        country
-        state
-      }
+      actionSuccessful
+      message
+      userToken
     }
   }
 `
